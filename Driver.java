64c313b5551190a65319ad5059ac119f3963f757
AlonzoManaog <116593302+AlonzoManaog@users.noreply.github.com>
2022-12-11 08:29:54 -0500

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args){
+        MyDate date= new MyDate();
+        date.setDate(17,7,2022);; 
+        System.out.println(date.toString());
+    }
+}
